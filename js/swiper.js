c8eb571a16618770fdaf8aa324b15c69c7a02c93
SherimbekovAzat <sherimbekov.azat@bk.ru>
2021-03-29 23:58:04 +0600

    I added a new project

---
+++ js/swiper.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper(".swiper-container", {
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  autoplay: {
+    delay: 1500,
+  },
+  loop: true,
+  slidesPerView: 3,
+  slidesPerGroup: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+const swiper = new Swiper(".swiper-container", {
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  autoplay: {
+    delay: 1500,
+  },
+  loop: true,
+  slidesPerView: 3,
+  slidesPerGroup: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
